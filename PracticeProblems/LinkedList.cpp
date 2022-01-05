8da8ee3c5a9cb105ddd4c8980ceca5b326d44aa8
Happy <poddarsayan22@gmail.com>
2022-01-05 13:27:46 +0530

    Add prepend and display func

--- PracticeProblems/LinkedList.cpp
+++ PracticeProblems/LinkedList.cpp
@@ -3,25 +3,61 @@ using namespace std;
 
 typedef struct Node{
     int data;
-    struct node* next;
+    struct Node *next;
 }Node;
 
 int main(){
-    Node * start = NULL;
-    int ch;
+    void prepend(Node**, int);
+    void displayList(Node *);
+    Node* start = NULL;
+    int ch, value;
     while(1){
-        cout << "1:Prepend\t\t2:Append\t\t3:After a value\n";
+        cout << "\n\n1:Prepend\t\t2:Append\t\t3:After a value\n";
 
-        cout<<"4:Display\t\t5:Search\t\t6:Delete\n";
+        cout << "4:Display\t\t5:Search\t\t6:Delete\n";
+
+        cout << "7:Exit\n\n";
+        cout << "Enter any value from the above menu : ";
         cin >> ch;
         switch(ch){
             case 1:
+                  cout<<"\nEnter data to prepend in the list\n";
+                  cin >> value;
+                  prepend(&start, value);
+                  break;
             case 2:
+                  break;
             case 3:
+                  break;
             case 4:
+                  displayList(start);
+                  break;
             case 5:
-            default: cout << "Wrong Input";
+                  break;
+            case 7: 
+                  return 0;
+            default: cout << "\nWrong Input\n";
         }
-        
+    }
+}
+
+void prepend(Node** start, int value){
+    Node* newnode = new Node; //create and allocate new node
+
+    newnode->data = value;
+    newnode->next = (*start);
+
+    (*start) = newnode;
+
+}
+
+void displayList(Node *node){
+    cout << "\n\n";
+    while(node != NULL){
+        cout << node->data << "--->";
+        node = node->next;
+    }
+    if(node == NULL){
+        cout << "NULL\n\n";
     }
 }
