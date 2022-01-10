72625d156db3568dba31d6456c61ed0e0747e59a
Happy <poddarsayan22@gmail.com>
2022-01-11 00:05:52 +0530

    Initiate Heap

---
+++ PracticeProblems/Heap.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ch;
+    while(1){
+        cout << "\n\t\t\tMENU\t\t\t\n";
+        cout << "1.Insert Elements\t\t2.Display Elements\n";
+        cout << "3.Build Heap\t\t4.Heap Sort\n";
+        cout << "5.Exit\n\n";
+        cout << "Enter any value from the menu : ";
+        cin >> ch;
+
+        switch(ch){
+            case 1:
+                 break;
+            case 2:
+                 break;
+            case 3:
+                 break;
+            case 4:
+                 break;
+            case 5:
+                 break;
+            default: 
+                 break;
+        }
+    }
+}
